76583cb0f8181acef13d1c9748166cb70d171206
unsi <unsi@list.ru>
2015-10-22 19:45:44 +0300

    new feature

--- Str.java
+++ Str.java
@@ -6,13 +6,17 @@ class Str
 	public static void main(String[] args){
 		try{
 		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a number of the words:");
 		int num = Integer.parseInt(r.readLine());
 		for(int i = 0; i < num; i ++){
+			System.out.println("Enter the word:");
 			String s = r.readLine();
-			if (s.length()>3)
-			s = s.replace(' ', '2');
-			else System.out.println("Меньше 3-х символов в слове");
-			System.out.println(s);
+			if (s.length()>=8)
+			{
+				s=s.substring(3,8); //3-й символ игнорируется
+				System.out.println(s);
+			}
+			else System.out.println("Less than 8 symbols in the word");
 		}
 		}catch (Exception e) {}
 	
